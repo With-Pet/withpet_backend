0bdb68ae59fa7bccdac8d98cbd0cd448ced0082a
HyungJooKim <hyungjookim12@gmail.com>
2022-03-11 20:42:19 +0900

    [:wrench: config] 초기 세팅 구성

---
+++ src/main/java/com/example/withpet_01/Withpet01Application.java
@@ -0,0 +1,13 @@
+package com.example.withpet_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Withpet01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Withpet01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/withpet_01/Withpet01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.withpet_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Withpet01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
