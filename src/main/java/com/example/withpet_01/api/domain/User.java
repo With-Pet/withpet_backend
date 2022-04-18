d5282528272ca7b750727f2b7c827f4f212c17c1
SeungYoung Oh <seung-00@naver.com>
2022-04-18 23:19:47 +0900

    refactor: User domain 수정

--- src/main/java/com/example/withpet_01/api/domain/User.java
+++ src/main/java/com/example/withpet_01/api/domain/User.java
@@ -4,7 +4,6 @@ import com.example.withpet_01.api.domain.common.CommonDateEntity;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
-import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -16,7 +15,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Entity
 @Getter
@@ -27,53 +25,48 @@ import java.util.stream.Collectors;
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 @Table(name = "user")
 public class User extends CommonDateEntity implements UserDetails  {
-
-    @Id // pk
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id @GeneratedValue // 어떤 시퀸스 값
     @Column(name = "user_id")
-    private Long uNum;
+    private Long id;
 
-    @Column(nullable = false, unique = true, length = 50) // SNS ID
-    private String id;
-    @Setter
-    @Column(nullable = true, unique = true) // 닉네임
-    private String name;
-    @Column(nullable = true)
+    @Column(name="sns_id", nullable = false, unique = true, length = 50) // SNS ID
+    private String snsId;
+
+    @Column(nullable =false, unique = true) // 닉네임
+    private String nickname;
+
+    @Column(name="phone_num", unique = true)
     private String phoneNum;
+
     @Email
-    @Column(nullable = true)
+    @Column
     private String email;
+
     @Column(length = 100)
     private String provider;
 
     @Column(length = 100)
-    private String searchLocation; //검색용 주소
+    private String address; //검색용 주소
 
     @Column(length = 1000)
-    private String searchX; //검색용 x좌표
+    private float x; //검색용 x좌표
 
     @Column(length = 1000)
-    private String searchY; //검색용 y좌표
+    private float y; //검색용 y좌표
 
 //    @JsonIgnore // 일단은..
 //    @OneToMany(mappedBy = "owner")
 //    private List<Pet> pets = new ArrayList<>();
 
-//    @JsonIgnore // 일단은..
-//    @OneToMany(mappedBy = "postAuthor")
-//    private List<Post> posts = new ArrayList<>();
+    @OneToMany(mappedBy = "user") // 연관관계 주인은 Post 의 user (FK)
+    private List<Post> posts = new ArrayList<>();
 
+    private String usrToken; //회원 토큰
 
     @ElementCollection(fetch = FetchType.EAGER)
     @Builder.Default
     private List<String> roles = new ArrayList<>();
 
-    private String usrToken; //회원 토큰
-
-    public User update(String name){
-        this.name = name;
-        return this;
-    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
