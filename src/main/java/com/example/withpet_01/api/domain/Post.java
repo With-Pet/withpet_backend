b72fe4bacdbc427064e019699ba3c31222e5d004
SeungYoung Oh <seung-00@naver.com>
2022-04-18 23:20:05 +0900

    refactor: Post domain 수정

--- src/main/java/com/example/withpet_01/api/domain/Post.java
+++ src/main/java/com/example/withpet_01/api/domain/Post.java
@@ -19,67 +19,53 @@ import java.util.List;
 @Table(name = "post")
 public class Post extends CommonDateEntity {
 
-    @Id
-    @Column(length = 5, nullable = false, unique = true)
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;                 //게시물 고유 번호
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_id", length = 5, nullable = false, unique = true)
+    private long id;                 //게시물 고유 번호
+
+    @Column(length = 1, nullable = false)
+    private PostType type;
+
+    @Column(nullable = false, length = 100)
+    private String title;       //게시물 제목
+
+    @Column(nullable = false)
+    private long price;       //시간당 이용 금액
+
+    @Column(length = 100,
+            nullable = false)
+    private String address;    //게시물 지역 명
+
+    @Column(length = 1000)
+    private String description;   //요청 사항
+
+    @Column(length = 100, nullable = false)
+    private LocalDateTime startTime;    //시작 날짜
+
+    @Column(length = 100, nullable = false)
+    private LocalDateTime endTime;    //종료 날짜
+
+    @Column(length = 1000, nullable = false)
+    private float x;    //위도
+
+    @Column(length = 1000, nullable = false)
+    private float y;    //경도
+
+    @Column(length = 1, nullable = false)
+    private boolean isFavorite;    //즐겨찾기 여부 TRUE : O , FALSE : X default : FALSE
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;              //작성자 정보
 
-    @Column(length = 50, nullable = false)
-    private String postAuthor;      //작성자 이름
-
-    @Column(length = 1, nullable = false)
-    private String postType;        // 산책 : W, 돌봄 : C, 체험 : E
-//
-//    @JoinColumn(name = "pet_id")
-//    @OneToOne
-//    private Pet pet;                //게시물 펫
-
-    @Column(nullable = false, length = 100)
-    private String postTitle;       //게시물 제목
-
-    @Column(length = 50)
-    private String postPrice;       //시간당 이용 금액
-
-    @Column(length = 100, nullable = false)
-    private String postLocation;    //게시물 지역 명
-
 //    @OneToOne(fetch = FetchType.LAZY)
 //    @JoinColumn(name = "service_id")
 //    private Service service;        //서비스 상세
-
-    @Column(length = 1000)
-    private String postRequested;   //요청 사항
 
 //    //하나의 게시글이 여러 파일을 가지므로 N(postImage) : 1(post) 관계가 된다.
 //    @OneToMany(fetch = FetchType.LAZY)
 //    @JoinColumn(name = "image_id")
 //    private List<Image> postImage = new ArrayList<>();    //게시물 사진
-
-    @Column(length = 100, nullable = false)
-    private LocalDateTime postSTime;    //시작 날짜
-
-    @Column(length = 100, nullable = false)
-    private LocalDateTime postETime;    //종료 날짜
-
-    @Column(length = 1000, nullable = false)
-    private String x;    //위도
-
-    @Column(length = 1000, nullable = false)
-    private String y;    //경도
-
-    @Column(length = 1, nullable = false)
-    private boolean postFav;    //즐겨찾기 여부 TRUE : O , FALSE : X default : FALSE
-
-
-    public Post setUpdate(String postAuthor, String postTitle) {
-        this.postAuthor = postAuthor;
-        this.postTitle = postTitle;
-        return this;
-    }
 
 //    // Post 에서 파일 처리 위함
 //    public void addPhoto(Image postImage) {
---
+++ src/main/java/com/example/withpet_01/api/domain/PostType.java
@@ -0,0 +1,6 @@
+package com.example.withpet_01.api.domain;
+
+public enum PostType {
+    // 산책 : W, 돌봄 : C, 체험 : E
+    W, C, E
+}
